650cd4f0551f0584501646778ce36c6a743cb929
Lorenzo Feng <paradise_c@qq.com>
2025-03-16 07:11:36 +0800

    post new blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://lorenzofeng.cc",
+  site: "https://7emotions.atomgit.net",
   base: "/blog",
   trailingSlash: "always",
   integrations: [
