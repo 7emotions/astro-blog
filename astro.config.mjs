f30e0daa313dc4325c689b2493d71ede97b8165c
Lorenzo Feng <paradise_c@qq.com>
2025-03-15 17:50:22 +0800

    domain change

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://7emotions.atomgit.net",
-  base: "/blog",
+  site: "http://lorenzofeng.top/",
+  // base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(
