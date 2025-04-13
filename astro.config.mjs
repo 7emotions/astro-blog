dd7a61556704ce424cf3f5d60f982031f8d66f26
Lorenzo Feng <paradise_c@qq.com>
2025-04-13 12:56:11 +0800

    merge atomgit into vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lorenzofeng.top",
+  site: "https://lorenzofeng.vercel.app",
   base: "/",
   trailingSlash: "always",
   integrations: [
